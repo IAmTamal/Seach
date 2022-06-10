7706e9835bba0a44d3f7aa1b767e785c02e03411
Tamal Das <gyansujan69@gmail.com>
2022-06-10 22:11:41 +0530

    Fixed APIs

---
+++ pages/api/products/addtodolisttodb.js
@@ -0,0 +1,49 @@
+import connectDb from "../../../middleware/db";
+import Users from "../../../models/UserSchema";
+
+const handler = async (req, res) => {
+  console.log(req.body);
+
+  const { email, username, todolist } = req.body;
+  const newuser = {};
+
+  let searcheduser = await Users.findOne({ email: email });
+
+  if (searcheduser) {
+    if (email) {
+      newuser.email = email;
+    }
+
+    if (username) {
+      newuser.username = username;
+    }
+
+    if (todolist) {
+      newuser.todolist = todolist;
+    }
+
+    let u = await Users.findOneAndUpdate({ email: email }, newuser);
+
+    return res.status(200).json({ sucess: "sucess" });
+  } else {
+    if (email) {
+      newuser.email = email;
+    }
+
+    if (username) {
+      newuser.username = username;
+    }
+
+    if (todolist) {
+      newuser.todolist = todolist;
+    }
+
+    let p = new Users(newuser);
+
+    await p.save();
+
+    return res.status(200).json(p);
+  }
+};
+
+export default connectDb(handler);
